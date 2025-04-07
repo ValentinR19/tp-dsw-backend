3085377c6b222c963dd380bc622136e605c5f171
Valentin Rodriguez <vrodriguez.crucijuegos@gmail.com>
2025-04-07 12:22:34 -0300

    Se agregan las dependencias principales y la dockerizacion del proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
+import appConfig from '@config-module/app.config';
+import databaseConfig from '@config-module/database.config';
+import { validationSchema } from '@config-module/validation.schema';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      load: [appConfig, databaseConfig],
+      validationSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('host'),
+        port: configService.get<number>('port'),
+        username: configService.get<string>('username'),
+        password: configService.get<string>('password'),
+        database: configService.get<string>('database'),
+        entities: ['dist/**/models/*/*{.entity.ts,.entity.js}'],
+        synchronize: false,
+        autoLoadEntities: true,
+        logging: false,
+        extra: {
+          timezone: 'local',
+        },
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalPipes(new ValidationPipe({ transform: true, stopAtFirstError: true }));
+  app.use(helmet());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
