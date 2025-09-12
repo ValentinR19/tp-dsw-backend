f615cf4827a9d57233aba8e1c7e07ceb163633f5
joaquingarciaf9 <joaquingarciaforestello@gmail.com>
2025-09-12 09:31:11 -0300

    update productdto

--- src/products/models/dtos/update-product.dto.ts
+++ src/products/models/dtos/update-product.dto.ts
@@ -3,25 +3,23 @@ import { IsNumber, IsString, IsOptional, Length, Min, IsInt} from 'class-validat
 export class UpdateProductDto {
 @IsOptional()
 @IsString()
-@Length(1, 120, { message: 'name debe tener entre 1 y 120 caracteres' })
+@Length(1, 120, { message: 'El nombre debe tener entre 1 y 120 caracteres' })
 name!: string;
 
 @IsOptional()
 @IsString()
-@Length(0, 1000, { message: 'description no debe exceder 1000 caracteres' })
+@Length(0, 1000, { message: 'La description no debe exceder 1000 caracteres' })
 description?: string;
 
 @IsOptional()
-@IsInt({ message: 'productCategoryId debe ser un entero' })
-@Min(1, { message: 'productCategoryId debe ser >= 1' })
+@IsInt({ message: 'El id de producto debe ser un entero' })
+@Min(1, { message: 'El id de producto debe ser >= 1' })
 productCategoryId!: number;
 
 @IsOptional()
 @IsNumber(
 { allowInfinity: false, allowNaN: false, maxDecimalPlaces: 2 },
-{ message: 'price debe ser un número con hasta 2 decimales' })
-
-@IsOptional()
-@Min(0, { message: 'price no puede ser negativo' })
+{ message: 'El precio debe ser un número con hasta 2 decimales' })
+@Min(0, { message: 'El precio no puede ser negativo' })
 price?: number;
 }
