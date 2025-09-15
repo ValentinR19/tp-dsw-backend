f12707a18aa30fac05dcf95f63362042d0d8e898
ruciso20 <guille.petri47@gmail.com>
2025-09-15 19:49:00 -0300

    update dtos de product y product.service

--- src/products/models/dtos/create-product.dto.ts
+++ src/products/models/dtos/create-product.dto.ts
@@ -1,24 +1,19 @@
-import { IsNumber, IsString, IsOptional, Length, Min, IsInt} from 'class-validator';
+import { IsCurrency, IsDecimal, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateProductDto {
   @IsString()
-  @Length(1, 120, { message: 'El nombre debe tener entre 1 y 120 caracteres' })
-  name!: string;
+  @IsNotEmpty()
+  name: string;
 
-  @IsOptional()
   @IsString()
-  @Length(0, 1000, { message: 'La description no debe exceder 1000 caracteres' })
-  description?: string;
+  description: string;
 
-  
-  @IsInt({ message: 'El id de producto debe ser un entero' })
-  @Min(1, { message: 'EL id de producto debe ser >= 1' })
-  productCategoryId!: number;
+  @IsNumber()
+  productCategoryId: number;
 
-  @IsOptional()
-  @IsNumber(
-    { allowInfinity: false, allowNaN: false, maxDecimalPlaces: 2 },
-    { message: 'El precio debe ser un número con hasta 2 decimales' })
-  @Min(0, { message: 'El precio no puede ser negativo' })
+  @IsDecimal()
   price?: number;
+
+  @IsCurrency()
+  currency?: string;
 }
--- src/products/models/dtos/update-product.dto.ts
+++ src/products/models/dtos/update-product.dto.ts
@@ -1,21 +1,4 @@
-import { IsNumber, IsString, IsOptional, Length, Min, IsInt} from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateProductDto } from '@product-module/models/dtos/create-product.dto';
 
-export class UpdateProductDto {
-@IsOptional()
-@IsString()
-@Length(1, 120, { message: 'El nombre debe tener entre 1 y 120 caracteres' })
-name!: string;
-
-@IsOptional()
-@IsString()
-@Length(0, 1000, { message: 'La description no debe exceder 1000 caracteres' })
-description?: string;
-
-
-@IsOptional()
-@IsNumber(
-{ allowInfinity: false, allowNaN: false, maxDecimalPlaces: 2 },
-{ message: 'El precio debe ser un número con hasta 2 decimales' })
-@Min(0, { message: 'El precio no puede ser negativo' })
-price?: number;
-}
+export class UpdateProductDto extends PartialType(CreateProductDto) { }
